3575cbf2ea4ec4efb5a077c08323fed32c4d22ed
Noah <103532278+noahanutta@users.noreply.github.com>
2024-08-02 09:53:18 -0700

    init

--- dist/index.ce70da86.js
+++ dist/index.ce70da86.js
@@ -142,14 +142,14 @@
       this[globalName] = mainExports;
     }
   }
-})({"akGAW":[function(require,module,exports) {
+})({"5RpUT":[function(require,module,exports) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
-module.bundle.HMR_BUNDLE_ID = "c5968d2e8199921f";
+module.bundle.HMR_BUNDLE_ID = "74798c74ce70da86";
 "use strict";
 /* global HMR_HOST, HMR_PORT, HMR_ENV_HASH, HMR_SECURE, HMR_USE_SSE, chrome, browser, __parcel__import__, __parcel__importScripts__, ServiceWorkerGlobalScope */ /*::
 import type {
@@ -583,37 +583,466 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
     });
 }
 
-},{}],"gkKU3":[function(require,module,exports) {
-exports.interopDefault = function(a) {
-    return a && a.__esModule ? a : {
-        default: a
-    };
-};
-exports.defineInteropFlag = function(a) {
-    Object.defineProperty(a, "__esModule", {
-        value: true
+},{}],"chb94":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+var _lenis = require("@studio-freight/lenis");
+var _lenisDefault = parcelHelpers.interopDefault(_lenis);
+var _gsap = require("gsap");
+var _scrollTrigger = require("gsap/ScrollTrigger");
+var _splittingCss = require("splitting/dist/splitting.css");
+var _splittingCellsCss = require("splitting/dist/splitting-cells.css");
+var _splitting = require("splitting");
+var _splittingDefault = parcelHelpers.interopDefault(_splitting);
+(0, _gsap.gsap).registerPlugin((0, _scrollTrigger.ScrollTrigger));
+const wrapElements = (t, e, r)=>{
+    t.forEach((t)=>{
+        const o = document.createElement(e);
+        o.classList = r, t.parentNode.appendChild(o), o.appendChild(t);
     });
 };
-exports.exportAll = function(source, dest) {
-    Object.keys(source).forEach(function(key) {
-        if (key === "default" || key === "__esModule" || Object.prototype.hasOwnProperty.call(dest, key)) return;
-        Object.defineProperty(dest, key, {
-            enumerable: true,
-            get: function() {
-                return source[key];
+(0, _splittingDefault.default)();
+const letterRoll = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-letter-roll]")
+], letterStretch = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-letter-stretch]")
+], letterFlip = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-letter-flip]")
+], compactor = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-compactor]")
+], fadedSort = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-faded-sort]")
+], letterRise = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-letter-rise]")
+], sideRoll = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-side-roll]")
+], obfText = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-obf-text]")
+], fizzLetters = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-fizz-letters]")
+], fadePop = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-fade-pop]")
+], letterFall = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-letter-fall]")
+], scatterFlip = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-scatter-flip]")
+], growIn = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-grow-in]")
+], fangs = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-fangs]")
+], ghost = [
+    ...document.querySelectorAll(".content__title[data-splitting][data-ghost]")
+];
+let lenis;
+const initSmoothScrolling = ()=>{
+    lenis = new (0, _lenisDefault.default)({
+        lerp: .2,
+        smooth: !0
+    }), lenis.on("scroll", ()=>(0, _scrollTrigger.ScrollTrigger).update());
+    const t = (e)=>{
+        lenis.raf(e), requestAnimationFrame(t);
+    };
+    requestAnimationFrame(t);
+}, scroll = ()=>{
+    fadePop.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        e.forEach((t)=>(0, _gsap.gsap).set(t.parentNode, {
+                perspective: 1e3
+            })), (0, _gsap.gsap).fromTo(e, {
+            "will-change": "opacity, transform",
+            opacity: .2,
+            z: -800
+        }, {
+            ease: "back.out(1.2)",
+            opacity: 1,
+            z: 0,
+            stagger: .04,
+            scrollTrigger: {
+                trigger: t,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: !0
+            }
+        });
+    }), letterFall.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        e.forEach((t)=>(0, _gsap.gsap).set(t.parentNode, {
+                perspective: 1e3
+            })), (0, _gsap.gsap).fromTo(e, {
+            "will-change": "opacity, transform",
+            transformOrigin: "50% 0%",
+            opacity: 0,
+            rotationX: -90,
+            z: -200
+        }, {
+            ease: "power1",
+            opacity: 1,
+            stagger: .05,
+            rotationX: 0,
+            z: 0,
+            scrollTrigger: {
+                trigger: t,
+                start: "center bottom",
+                end: "bottom top+=20%",
+                scrub: !0
+            }
+        });
+    }), scatterFlip.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        e.forEach((t)=>(0, _gsap.gsap).set(t.parentNode, {
+                perspective: 1e3
+            })), (0, _gsap.gsap).fromTo(e, {
+            "will-change": "opacity, transform",
+            transformOrigin: "50% 100%",
+            opacity: 0,
+            rotationX: 90
+        }, {
+            ease: "power4",
+            opacity: 1,
+            stagger: {
+                each: .03,
+                from: "random"
+            },
+            rotationX: 0,
+            scrollTrigger: {
+                trigger: t,
+                start: "center bottom",
+                end: "bottom top+=20%",
+                scrub: !0
+            }
+        });
+    }), growIn.forEach((t)=>{
+        const e = [
+            ...t.querySelectorAll(".word")
+        ];
+        for (const [t, r] of e.entries())(0, _gsap.gsap).fromTo(r.querySelectorAll(".char"), {
+            "will-change": "transform",
+            scale: .01,
+            x: (e, r, o)=>t % 2 ? 50 * e : -50 * (o.length - e - 1)
+        }, {
+            ease: "power4",
+            scale: 1,
+            x: 0,
+            scrollTrigger: {
+                trigger: r,
+                start: "center bottom",
+                end: "bottom top-=40%",
+                scrub: !0
+            }
+        });
+    }), fangs.forEach((t)=>{
+        const e = [
+            ...t.querySelectorAll(".word")
+        ], r = (0, _gsap.gsap).timeline({
+            scrollTrigger: {
+                trigger: t,
+                start: "center center",
+                end: "+=100%",
+                scrub: !0,
+                pin: t.parentNode
+            }
+        });
+        for (const [t, o] of e.entries())r.fromTo(o.querySelectorAll(".char"), {
+            "will-change": "transform",
+            transformOrigin: ()=>!t % 2 ? "50% 0%" : "50% 100%",
+            scaleY: 0
+        }, {
+            ease: "power1.inOut",
+            scaleY: 1,
+            stagger: {
+                amount: .3,
+                from: "center"
+            }
+        }, 0);
+    }), ghost.forEach((t)=>{
+        const e = [
+            ...t.querySelectorAll(".word")
+        ];
+        for (const t of e){
+            const e = t.querySelectorAll(".char"), r = e.length;
+            (0, _gsap.gsap).fromTo(e, {
+                "will-change": "transform, filter",
+                transformOrigin: "50% 100%",
+                scale: (t)=>{
+                    const e = t < Math.ceil(r / 2) ? t : Math.ceil(r / 2) - Math.abs(Math.floor(r / 2) - t) - 1;
+                    return (0, _gsap.gsap).utils.mapRange(0, Math.ceil(r / 2), .5, 2.1, e);
+                },
+                y: (t)=>{
+                    const e = t < Math.ceil(r / 2) ? t : Math.ceil(r / 2) - Math.abs(Math.floor(r / 2) - t) - 1;
+                    return (0, _gsap.gsap).utils.mapRange(0, Math.ceil(r / 2), 0, 60, e);
+                },
+                rotation: (t)=>{
+                    const e = t < Math.ceil(r / 2) ? t : Math.ceil(r / 2) - Math.abs(Math.floor(r / 2) - t) - 1;
+                    return t < r / 2 ? (0, _gsap.gsap).utils.mapRange(0, Math.ceil(r / 2), -4, 0, e) : (0, _gsap.gsap).utils.mapRange(0, Math.ceil(r / 2), 0, 4, e);
+                },
+                filter: "blur(12px) opacity(0)"
+            }, {
+                ease: "power2.inOut",
+                y: 0,
+                rotation: 0,
+                scale: 1,
+                filter: "blur(0px) opacity(1)",
+                scrollTrigger: {
+                    trigger: t,
+                    start: "top bottom+=40%",
+                    end: "top top+=15%",
+                    scrub: !0
+                },
+                stagger: {
+                    amount: .15,
+                    from: "center"
+                }
+            });
+        }
+    }), letterRoll.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        (0, _gsap.gsap).fromTo(e, {
+            "will-change": "opacity, transform",
+            opacity: 0,
+            scale: .6,
+            rotationZ: ()=>(0, _gsap.gsap).utils.random(-20, 20)
+        }, {
+            ease: "power4",
+            opacity: 1,
+            scale: 1,
+            rotation: 0,
+            stagger: .4,
+            scrollTrigger: {
+                trigger: t,
+                start: "center+=20% bottom",
+                end: "+=50%",
+                scrub: !0
+            }
+        });
+    }), letterStretch.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        (0, _gsap.gsap).fromTo(e, {
+            "will-change": "opacity, transform",
+            opacity: 0,
+            yPercent: 120,
+            scaleY: 2.3,
+            scaleX: .7,
+            transformOrigin: "50% 0%"
+        }, {
+            duration: 1,
+            ease: "back.inOut(2)",
+            opacity: 1,
+            yPercent: 0,
+            scaleY: 1,
+            scaleX: 1,
+            stagger: .03,
+            scrollTrigger: {
+                trigger: t,
+                start: "center bottom+=50%",
+                end: "bottom top+=40%",
+                scrub: !0
+            }
+        });
+    }), letterFlip.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        (0, _gsap.gsap).fromTo(e, {
+            "will-change": "transform",
+            transformOrigin: "50% 0%",
+            scaleY: 0
+        }, {
+            ease: "back",
+            opacity: 1,
+            scaleY: 1,
+            yPercent: 0,
+            stagger: .03,
+            scrollTrigger: {
+                trigger: t,
+                start: "center bottom-=5%",
+                end: "top top-=20%",
+                scrub: !0
+            }
+        });
+    }), compactor.forEach((t)=>{
+        const e = t.querySelectorAll(".word");
+        for (const t of e){
+            const e = t.querySelectorAll(".char");
+            (0, _gsap.gsap).fromTo(e, {
+                "will-change": "opacity, transform",
+                x: (t, e, r)=>150 * (t - r.length / 2)
+            }, {
+                ease: "power1.inOut",
+                x: 0,
+                stagger: {
+                    grid: "auto",
+                    from: "center"
+                },
+                scrollTrigger: {
+                    trigger: t,
+                    start: "center bottom+=30%",
+                    end: "top top+=15%",
+                    scrub: !0
+                }
+            });
+        }
+    }), fadedSort.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        (0, _gsap.gsap).fromTo(e, {
+            "will-change": "opacity, transform",
+            opacity: 0,
+            xPercent: ()=>(0, _gsap.gsap).utils.random(-200, 200),
+            yPercent: ()=>(0, _gsap.gsap).utils.random(-150, 150)
+        }, {
+            ease: "power1.inOut",
+            opacity: 1,
+            xPercent: 0,
+            yPercent: 0,
+            stagger: {
+                each: .05,
+                grid: "auto",
+                from: "random"
+            },
+            scrollTrigger: {
+                trigger: t,
+                start: "center bottom+=40%",
+                end: "bottom center",
+                scrub: .9
+            }
+        });
+    }), letterRise.forEach((t)=>{
+        const e = t.querySelectorAll(".word");
+        for (const t of e){
+            const e = t.querySelectorAll(".char");
+            e.forEach((t)=>(0, _gsap.gsap).set(t.parentNode, {
+                    perspective: 2e3
+                })), (0, _gsap.gsap).fromTo(e, {
+                "will-change": "opacity, transform",
+                opacity: 0,
+                rotationX: -90,
+                yPercent: 50
+            }, {
+                ease: "power1.inOut",
+                opacity: 1,
+                rotationX: 0,
+                yPercent: 0,
+                stagger: {
+                    each: .03,
+                    from: 0
+                },
+                scrollTrigger: {
+                    trigger: t,
+                    start: "center bottom+=40%",
+                    end: "bottom center-=30%",
+                    scrub: .9
+                }
+            });
+        }
+    }), sideRoll.forEach((t)=>{
+        const e = t.querySelectorAll(".word");
+        for (const t of e){
+            const e = t.querySelectorAll(".char");
+            e.forEach((t)=>(0, _gsap.gsap).set(t.parentNode, {
+                    perspective: 2e3
+                })), (0, _gsap.gsap).fromTo(e, {
+                "will-change": "opacity, transform",
+                transformOrigin: "100% 50%",
+                opacity: 0,
+                rotationY: -90,
+                z: -300
+            }, {
+                ease: "expo",
+                opacity: 1,
+                rotationY: 0,
+                z: 0,
+                stagger: {
+                    each: .06,
+                    from: "end"
+                },
+                scrollTrigger: {
+                    trigger: t,
+                    start: "bottom bottom+=20%",
+                    end: "bottom top",
+                    scrub: 1
+                }
+            });
+        }
+    });
+    const t = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z"
+    ];
+    obfText.forEach((e)=>{
+        e.querySelectorAll(".char").forEach((r, o)=>{
+            let a = r.innerHTML;
+            (0, _gsap.gsap).fromTo(r, {
+                opacity: 0
+            }, {
+                duration: .03,
+                innerHTML: ()=>t[Math.floor(Math.random() * t.length)],
+                repeat: 1,
+                repeatRefresh: !0,
+                opacity: 1,
+                repeatDelay: .03,
+                delay: .18 * (o + 1),
+                onComplete: ()=>(0, _gsap.gsap).set(r, {
+                        innerHTML: a,
+                        delay: .03
+                    }),
+                scrollTrigger: {
+                    trigger: e,
+                    start: "top bottom",
+                    end: "bottom center",
+                    toggleActions: "play resume resume reset",
+                    onEnter: ()=>(0, _gsap.gsap).set(r, {
+                            opacity: 0
+                        })
+                }
+            });
+        });
+    }), fizzLetters.forEach((t)=>{
+        const e = t.querySelectorAll(".char");
+        (0, _gsap.gsap).fromTo(e, {
+            "will-change": "opacity",
+            opacity: 0,
+            filter: "blur(20px)"
+        }, {
+            duration: .25,
+            ease: "power1.inOut",
+            opacity: 1,
+            filter: "blur(0px)",
+            stagger: {
+                each: .05,
+                from: "random"
+            },
+            scrollTrigger: {
+                trigger: t,
+                start: "top bottom",
+                end: "center center",
+                toggleActions: "play resume resume reset"
             }
         });
     });
-    return dest;
 };
-exports.export = function(dest, destName, get) {
-    Object.defineProperty(dest, destName, {
-        enumerable: true,
-        get: get
-    });
-};
+initSmoothScrolling(), scroll();
 
-},{}],"ggVJc":[function(require,module,exports) {
+},{"@studio-freight/lenis":"ggVJc","gsap":"fPSuC","gsap/ScrollTrigger":"7wnFk","splitting/dist/splitting.css":"3uR7n","splitting/dist/splitting-cells.css":"7jeGL","splitting":"77jB6","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"ggVJc":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "default", ()=>c);
@@ -1018,6 +1447,36 @@ module.exports.TinyEmitter = E;
         }, e;
     }();
 });
+
+},{}],"gkKU3":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, "__esModule", {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === "default" || key === "__esModule" || Object.prototype.hasOwnProperty.call(dest, key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
 
 },{}],"fPSuC":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
@@ -7267,7 +7726,7 @@ Observer.getById = function(id) {
 };
 _getGSAP() && gsap.registerPlugin(Observer);
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"77jB6":[function(require,module,exports) {
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"3uR7n":[function() {},{}],"7jeGL":[function() {},{}],"77jB6":[function(require,module,exports) {
 (function(global, factory) {
     module.exports = factory();
 })(this, function() {
@@ -7661,6 +8120,6 @@ _getGSAP() && gsap.registerPlugin(Observer);
     return Splitting;
 });
 
-},{}]},["akGAW"], null, "parcelRequire2524")
+},{}]},["5RpUT","chb94"], "chb94", "parcelRequire2524")
 
-//# sourceMappingURL=index2.8199921f.js.map
+//# sourceMappingURL=index.ce70da86.js.map
